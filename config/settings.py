a98114dafb74d91161c2dc771a0a0a0d950e9aab
sakeven <jc5930@sina.cn>
2016-09-01 11:21:25 +0800

    fix static file
    Signed-off-by: sakeven <jc5930@sina.cn>

--- config/settings.py
+++ config/settings.py
@@ -123,9 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 try:
     from .local_settings import *
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('chat.urls')),
+    url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT}),
 ]
